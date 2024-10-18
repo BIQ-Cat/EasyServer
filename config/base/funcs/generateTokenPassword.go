60c2fb31c9273286d4c51421889e0a32b816438c
AnDev58 <95691880+AnDev58@users.noreply.github.com>
2024-10-18 14:51:16 +0300

    Added test support (#2)
    * Added linting
    
    * fixed package name
    
    * added test support

--- config/base/funcs/generateTokenPassword.go
+++ config/base/funcs/generateTokenPassword.go
@@ -1,13 +1,21 @@
 package funcs
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()")
 
 func GenerateTokenPassword(length int) string {
 	b := make([]rune, length)
 	for i := range b {
-		b[i] = symbols[rand.Intn(len(symbols))]
+		letter, err := rand.Int(rand.Reader, big.NewInt(int64(len(symbols))))
+		if err != nil {
+			// Internal error?
+			panic(err)
+		}
+		b[i] = symbols[letter.Int64()]
 	}
 	return string(b)
 }
