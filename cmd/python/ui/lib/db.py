fb6d6cc82f143a4205464986b08644f63223c900
AnDev 58 <andev58@outlook.com>
2024-12-18 21:05:52 +0300

    format...

--- cmd/python/ui/lib/db.py
+++ cmd/python/ui/lib/db.py
@@ -20,7 +20,8 @@ class Database:
     def update_data(self, table: str, value, id, col_name: str):
         with self.conn.cursor() as cur:
             cur.execute(
-                f"UPDATE {table} SET {col_name} = % WHERE id = %s", (value, id) # trunk-ignore(bandit)
+                f"UPDATE {table} SET {col_name} = % WHERE id = %s", # trunk-ignore(bandit)
+                (value, id),  # trunk-ignore(bandit)
             )
             self.conn.commit()
 
